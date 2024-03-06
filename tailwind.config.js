1d643ccb93dc48fcf5222648f6bac00341934b21
guslizikri <zikrizg7@gmail.com>
2024-03-06 08:20:00 +0700

    finished page payment, profile, and orderhistory

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
       backgroundImage: {
         'hero-result': "url('/public/assets/img/bg-result.png')",
         'profile': "url('/public/assets/img/profile.png')",
+        'loyalty': "url('/public/assets/img/loyalpoint.png')",
       },
       boxShadow: {
         'shadow-blur': "inset 0 0 0 2000px rgba(27, 27, 27, 0.5)",
